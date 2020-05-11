9ac7f717516d1e55fe0fb66a842f7794bb269e12
Stephen Ball <stephen@drstephenball.com>
2020-05-11 14:40:35 +0100

    Asteroids now spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,14 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private GameObject _enemyContainer;
     [SerializeField] private GameObject[] powerups;
+    [SerializeField] private GameObject _asteroidPrefab;
+    [SerializeField] private GameObject _asteroidContainer;
     
     private bool _stopSpawning = false;
     void Start()
     {
         StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnAsteroidRoutine());
         StartCoroutine(SpawnPowerUpRoutine());
     }
 
@@ -28,7 +31,18 @@ public class SpawnManager : MonoBehaviour
             Vector3 posToSpawn = new Vector3(Random.Range(-9.5f, 9.5f), 6.5f, 0);
             GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn,Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(Random.Range(2.0f,4.0f));
+        }
+    }
+    
+    IEnumerator SpawnAsteroidRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-9.5f, 9.5f), 6.5f, 0);
+            GameObject newAsteroid = Instantiate(_asteroidPrefab, posToSpawn,Quaternion.identity);
+            newAsteroid.transform.parent = _asteroidContainer.transform;
+            yield return new WaitForSeconds(Random.Range(2.0f,4.0f));
         }
     }
 
